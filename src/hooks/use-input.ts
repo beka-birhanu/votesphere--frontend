97cd6b644b6584974fddd59bd44f273c23578e4d
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-06 23:30:39 +0300

    feat(auth): create useInput hook
    - Uses React reducer
    - Takes a validator as an argument
    - Returns value, changeHandler, blurHandler, resetHandler, and error (from the validator)

---
+++ src/hooks/use-input.ts
@@ -0,0 +1,42 @@
+import { useReducer } from 'react';
+
+const initialInputState = { value: '', isTouched: false };
+interface InputState {
+  value: string;
+  isTouched: boolean;
+}
+type Action = { type: 'INPUT' | 'BLUR' | 'RESET'; value: string };
+
+function inputStateReducer(state: InputState, action: Action): InputState {
+  if (action.type === 'INPUT') {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === 'BLUR') {
+    return { value: state.value, isTouched: true };
+  }
+  if (action.type === 'RESET') {
+    return { value: '', isTouched: false };
+  }
+  throw new Error('Unhandled action type');
+}
+
+function useInput(valueValidator: CallableFunction) {
+  function valueChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    dispatch({ type: 'INPUT', value: event.target.value });
+  }
+
+  function inputBlurHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    dispatch({ type: 'BLUR', value: event.target.value });
+  }
+
+  function resetHandler() {
+    dispatch({ type: 'RESET', value: 's' });
+  }
+
+  const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
+  const inputError = inputState.isTouched ? valueValidator(inputState.value) : '';
+
+  return [inputState.value, valueChangeHandler, inputBlurHandler, resetHandler, inputError];
+}
+
+export default useInput;
