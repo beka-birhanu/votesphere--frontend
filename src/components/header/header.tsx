6bab9c437030168756e020609cee4a8c52410807
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-06 16:43:25 +0300

    fix(style): align header items to center

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,14 +2,14 @@ import '../../index.css';
 import logo from '../../logo.jpg';
 
 const signOutLink = (
-  <a href='#' className='text-xl font-medium hover:underline'>
+  <a href='#' className='font-medium hover:underline'>
     Sign out
   </a>
 );
 
 function UnauthorizedHeader(props: { isAuthorized: boolean }) {
   return (
-    <header className='py-10 w-full px-9'>
+    <header className='py-10 w-full px-9 flex justify-between items-center md:pr-32 md:mb-32 mb-32 text-2xl'>
       <div className='flex items-end gap-3'>
         <img className='w-9' src={logo} alt='logo' />
         <h1 className='text-2xl uppercase font-bold tracking-wider'>Vote Sphere</h1>
