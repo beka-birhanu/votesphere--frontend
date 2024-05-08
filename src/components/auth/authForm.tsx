1d63f027e946fd291cdfb5da819938d79281d187
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-08 22:45:52 +0300

    style(auth): decrease error icon size in authForm

--- src/components/auth/authForm.tsx
+++ src/components/auth/authForm.tsx
@@ -5,7 +5,7 @@ import zxcvbn from 'zxcvbn';
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const usernameRegex = /^[a-zA-Z0-9_]+$/;
 const errorIcon = (
-  <svg width='24px' height='24px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <svg width='12px' height='12px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
     <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
     <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
     <g id='SVGRepo_iconCarrier'>
