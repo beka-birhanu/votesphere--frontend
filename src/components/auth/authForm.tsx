45370fd20029e88d5430f8442b70a075baa0f9f9
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-09 22:42:44 +0300

    refactor(auth): move error displaying to Input
    - Input takes error message from form parent component
       displays it

--- src/components/auth/authForm.tsx
+++ src/components/auth/authForm.tsx
@@ -4,19 +4,6 @@ import { validateEmail, validatePassword, validateUsername } from './validators'
 import { useEffect, useState } from 'react';
 import RadioButton from './authRadioButton';
 
-const errorIcon = (
-  <svg width='12px' height='12px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-    <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
-    <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
-    <g id='SVGRepo_iconCarrier'>
-      {' '}
-      <path d='M12 8V12' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></path>{' '}
-      <path d='M12 16.0195V16' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></path>{' '}
-      <circle cx='12' cy='12' r='10' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></circle>{' '}
-    </g>
-  </svg>
-);
-
 function submit(formData: { email: string; username: string; password: string }): null | string[] {
   // hit API
   const errors = ['ksld'];
@@ -49,67 +36,43 @@ function AuthForm(props: { type: 'login' | 'sign up' }) {
     return null;
   }
 
-  const emailInputWithError = (
-    <div className='flex flex-col gap-2 mb-6 md:mb-8'>
-      <Input
-        type='email'
-        changeHandler={setEmail}
-        blurHandler={emailInputBlurHandler}
-        value={email}
-        hasError={emailInputError !== null}
-        isTouched={isEmailInputTouched}
-      ></Input>
-      {emailInputError !== null && (
-        <div className='flex gap-1 pl-6'>
-          {errorIcon}
-          <p className='text-sm text-red-500'>{emailInputError?.toString()}</p>
-        </div>
-      )}
-    </div>
+  const emailInputField = (
+    <Input
+      type='email'
+      changeHandler={setEmail}
+      blurHandler={emailInputBlurHandler}
+      value={email}
+      error={emailInputError}
+      isTouched={isEmailInputTouched}
+    ></Input>
   );
-  const usernameInputWithError = (
-    <div className='flex flex-col gap-2 mb-6 md:mb-8'>
-      <Input
-        type='username'
-        changeHandler={setUsername}
-        blurHandler={usernameInputBlurHandler}
-        value={username}
-        hasError={usernameInputError !== null}
-        isTouched={isUsernameInputTouched}
-      ></Input>
 
-      {usernameInputError !== null && (
-        <div className='flex gap-1 pl-6 items-center'>
-          {errorIcon}
-          <p className='text-sm text-red-500'>{usernameInputError}</p>
-        </div>
-      )}
-    </div>
+  const usernameInputField = (
+    <Input
+      type='username'
+      changeHandler={setUsername}
+      blurHandler={usernameInputBlurHandler}
+      value={username}
+      error={usernameInputError}
+      isTouched={isUsernameInputTouched}
+    ></Input>
   );
-  const passwordInputWithError = (
-    <div className='flex flex-col gap-2 mb-6 md:mb-8'>
-      <Input
-        type='password'
-        changeHandler={setPassword}
-        blurHandler={passwordInputBlurHandler}
-        value={password}
-        hasError={passwordInputError !== null}
-        isTouched={isPasswordInputTouched}
-      ></Input>
 
-      {passwordInputError !== null && (
-        <div className='flex gap-1 pl-6 justify-items-center'>
-          {errorIcon}
-          <p className='text-sm text-red-500'>{passwordInputError}</p>
-        </div>
-      )}
-    </div>
+  const passwordInputField = (
+    <Input
+      type='password'
+      changeHandler={setPassword}
+      blurHandler={passwordInputBlurHandler}
+      value={password}
+      error={passwordInputError}
+      isTouched={isPasswordInputTouched}
+    ></Input>
   );
 
   return (
     <form className='flex flex-col justify-stretch gap-16' onSubmit={handleSubmit}>
       <div className='flex flex-col justify-stretch sm:p-12 p-2'>
-        {props.type === 'sign up' && emailInputWithError} {usernameInputWithError} {passwordInputWithError}
+        {props.type === 'sign up' && emailInputField} {usernameInputField} {passwordInputField}
         {<RadioButton onSelect={setRole} options={['User', 'Admin']}></RadioButton>}
       </div>
       <button className='rounded-lg bg-blue-700 py-3 text-blue-50 shadow-sm text-2xl disabled:bg-gray-500' disabled={formHasError}>
--- src/components/auth/authInput.tsx
+++ src/components/auth/authInput.tsx
@@ -8,6 +8,19 @@ const userIconSVG = <UserIconSVG />;
 const emailIconSVG = <EmailIconSVG />;
 const passwordIconSVG = <PasswordIconSVG />;
 
+const errorIcon = (
+  <svg width='12px' height='12px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+    <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
+    <g id='SVGRepo_iconCarrier'>
+      {' '}
+      <path d='M12 8V12' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></path>{' '}
+      <path d='M12 16.0195V16' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></path>{' '}
+      <circle cx='12' cy='12' r='10' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></circle>{' '}
+    </g>
+  </svg>
+);
+
 const nonErrorStyleClassName = 'border-[1px] border-[#CDCDCD] hover:border-gray-400';
 const errorStyleClassName = 'border-[1px] border-red-400 hover:border-red-400';
 
@@ -16,7 +29,7 @@ function Input(props: {
   changeHandler: ChangeEventHandler<HTMLInputElement>;
   blurHandler: FocusEventHandler<HTMLInputElement>;
   value: string;
-  hasError: boolean;
+  error: string | null;
   isTouched: boolean;
 }) {
   let icon;
@@ -30,23 +43,31 @@ function Input(props: {
   }
 
   return (
-    <div
-      className={`flex items-center text-lg border-solid  rounded-xl shadow-sm ${
-        props.hasError && props.isTouched ? errorStyleClassName : nonErrorStyleClassName
-      }`}
-    >
-      {icon}
+    <div className='flex flex-col gap-2 mb-6 md:mb-8'>
+      <div
+        className={`flex items-center text-lg border-solid  rounded-xl shadow-sm ${
+          props.error !== null && props.isTouched ? errorStyleClassName : nonErrorStyleClassName
+        }`}
+      >
+        {icon}
 
-      <input
-        type={props.type === 'username' ? 'text' : props.type}
-        placeholder={'Enter ' + props.type}
-        name={props.type}
-        id={props.type}
-        value={props.value}
-        onChange={props.changeHandler}
-        onBlur={props.blurHandler}
-        className='rounded pl-12 py-2 md:py-4 focus:outline-none w-full ml-5 text-2xl text-gray-700'
-      />
+        <input
+          type={props.type === 'username' ? 'text' : props.type}
+          placeholder={'Enter ' + props.type}
+          name={props.type}
+          id={props.type}
+          value={props.value}
+          onChange={props.changeHandler}
+          onBlur={props.blurHandler}
+          className='rounded pl-12 py-2 md:py-4 focus:outline-none w-full ml-5 text-2xl text-gray-700'
+        />
+      </div>
+      {props.error !== null && props.isTouched && (
+        <div className='flex gap-1 pl-6'>
+          {errorIcon}
+          <p className='text-sm text-red-500'>{props.error?.toString()}</p>
+        </div>
+      )}
     </div>
   );
 }
