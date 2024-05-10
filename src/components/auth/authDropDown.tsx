7926d8902f8743d113bcb4b686dd28a7a4a0c26c
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-10 12:03:54 +0300

    style: reformat with tab width 4

--- src/components/auth/authDropDown.tsx
+++ src/components/auth/authDropDown.tsx
@@ -1,26 +1,26 @@
 function DropDown(props: { onSelect: CallableFunction; options: string[] }) {
-  function handleSelection(event: React.ChangeEvent<HTMLSelectElement>) {
-    props.onSelect(event.target.value);
-  }
+    function handleSelection(event: React.ChangeEvent<HTMLSelectElement>) {
+        props.onSelect(event.target.value);
+    }
 
-  const options = props.options;
+    const options = props.options;
 
-  return (
-    <div className='flex justify-end items-center gap-2'>
-      <label htmlFor='role' className='text-xl'>
-        Role
-      </label>
-      <select name='role' id='role' className='text-gray-700 rounded-lg p-1 px-8 focus:outline-none text-xl' onChange={handleSelection}>
-        <option value={''} disabled selected hidden>
-          select role
-        </option>
-        {options.map((option) => (
-          <option value={option} key={option} className='text-xl'>
-            {option}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
+    return (
+        <div className='flex justify-end items-center gap-2'>
+            <label htmlFor='role' className='text-xl'>
+                Role
+            </label>
+            <select name='role' id='role' className='text-gray-700 rounded-lg p-1 px-8 focus:outline-none text-xl' onChange={handleSelection}>
+                <option value={''} disabled selected hidden>
+                    select role
+                </option>
+                {options.map((option) => (
+                    <option value={option} key={option} className='text-xl'>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
 }
 export default DropDown;
