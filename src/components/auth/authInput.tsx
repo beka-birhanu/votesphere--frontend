81abd6b8ef8c521f2d39836134524872e7486354
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-10 12:04:56 +0300

    style: reformat with tab width 4

--- src/components/auth/authInput.tsx
+++ src/components/auth/authInput.tsx
@@ -9,67 +9,67 @@ const emailIconSVG = <EmailIconSVG />;
 const passwordIconSVG = <PasswordIconSVG />;
 
 const errorIcon = (
-  <svg width='12px' height='12px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-    <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
-    <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
-    <g id='SVGRepo_iconCarrier'>
-      {' '}
-      <path d='M12 8V12' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></path>{' '}
-      <path d='M12 16.0195V16' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></path>{' '}
-      <circle cx='12' cy='12' r='10' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></circle>{' '}
-    </g>
-  </svg>
+    <svg width='12px' height='12px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+        <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
+        <g id='SVGRepo_iconCarrier'>
+            {' '}
+            <path d='M12 8V12' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></path>{' '}
+            <path d='M12 16.0195V16' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></path>{' '}
+            <circle cx='12' cy='12' r='10' stroke='#ef4444' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></circle>{' '}
+        </g>
+    </svg>
 );
 
 const nonErrorStyleClassName = 'border-[1px] border-[#CDCDCD] hover:border-gray-400';
 const errorStyleClassName = 'border-[1px] border-red-400 hover:border-red-400';
 
 function Input(props: {
-  type: 'username' | 'email' | 'password';
-  changeHandler: ChangeEventHandler<HTMLInputElement>;
-  blurHandler: FocusEventHandler<HTMLInputElement>;
-  value: string;
-  error: string | null;
-  isTouched: boolean;
+    type: 'username' | 'email' | 'password';
+    changeHandler: ChangeEventHandler<HTMLInputElement>;
+    blurHandler: FocusEventHandler<HTMLInputElement>;
+    value: string;
+    error: string | null;
+    isTouched: boolean;
 }) {
-  let icon;
+    let icon;
 
-  if (props.type == 'username') {
-    icon = userIconSVG;
-  } else if (props.type === 'email') {
-    icon = emailIconSVG;
-  } else if (props.type === 'password') {
-    icon = passwordIconSVG;
-  }
+    if (props.type == 'username') {
+        icon = userIconSVG;
+    } else if (props.type === 'email') {
+        icon = emailIconSVG;
+    } else if (props.type === 'password') {
+        icon = passwordIconSVG;
+    }
 
-  return (
-    <div className='flex flex-col gap-2 mb-6 md:mb-8'>
-      <div
-        className={`flex items-center text-lg border-solid  rounded-xl shadow-sm ${
-          props.error !== null && props.isTouched ? errorStyleClassName : nonErrorStyleClassName
-        }`}
-      >
-        {icon}
+    return (
+        <div className='flex flex-col gap-2 mb-6 md:mb-8'>
+            <div
+                className={`flex items-center text-lg border-solid  rounded-xl shadow-sm ${
+                    props.error !== null && props.isTouched ? errorStyleClassName : nonErrorStyleClassName
+                }`}
+            >
+                {icon}
 
-        <input
-          type={props.type === 'username' ? 'text' : props.type}
-          placeholder={'Enter ' + props.type}
-          name={props.type}
-          id={props.type}
-          value={props.value}
-          onChange={props.changeHandler}
-          onBlur={props.blurHandler}
-          className='rounded pl-12 py-2 md:py-4 focus:outline-none w-full ml-5 text-2xl text-gray-700'
-        />
-      </div>
-      {props.error !== null && props.isTouched && (
-        <div className='flex gap-1 pl-6'>
-          {errorIcon}
-          <p className='text-sm text-red-500'>{props.error?.toString()}</p>
+                <input
+                    type={props.type === 'username' ? 'text' : props.type}
+                    placeholder={'Enter ' + props.type}
+                    name={props.type}
+                    id={props.type}
+                    value={props.value}
+                    onChange={props.changeHandler}
+                    onBlur={props.blurHandler}
+                    className='rounded pl-12 py-2 md:py-4 focus:outline-none w-full ml-5 text-2xl text-gray-700'
+                />
+            </div>
+            {props.error !== null && props.isTouched && (
+                <div className='flex gap-1 pl-6'>
+                    {errorIcon}
+                    <p className='text-sm text-red-500'>{props.error?.toString()}</p>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default Input;
