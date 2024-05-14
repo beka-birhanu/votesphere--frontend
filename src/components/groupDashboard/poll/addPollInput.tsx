b5b891365e9d91879bab33333e9a36406ac4d0f6
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-14 23:11:42 +0300

    feat(poll): create add poll input fields from add-poll markup
    - has no input validation

---
+++ src/components/groupDashboard/poll/addPollInput.tsx
@@ -0,0 +1,12 @@
+function AddPollInput(props: { optionNumber: number }) {
+    return (
+        <div className='ml-6 flex flex-col sm:flex-row justify-items-stretch  '>
+            <label htmlFor='option1' className='mr-6'>
+                {' '}
+                Option {props.optionNumber}:
+            </label>
+            <input id='option1' type='text' className='border-b-2 border-gray-400 focus:outline-none flex-grow' multiple={true} />
+        </div>
+    );
+}
+export default AddPollInput;
