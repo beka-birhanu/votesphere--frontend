271bea36e394d7f4a84b05d23c636006f3420224
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-15 15:39:41 +0300

    fix(group): fix click listners in 'add member form
    - the form overlay closes only if the
       the background is clicked

--- src/components/groupDashboard/members/addMemberForm.tsx
+++ src/components/groupDashboard/members/addMemberForm.tsx
@@ -1,8 +1,9 @@
+import { MouseEventHandler } from 'react';
 import useInput from '../../../hooks/use-input';
 import Input from '../../auth/authInput';
 import { validateUsername } from '../../auth/validators';
 
-function AddMemberForm() {
+function AddMemberForm(props: { onClose: MouseEventHandler<HTMLElement> }) {
     const [username, isUsernameInputTouched, usernameInputError, setUsername, usernameInputBlurHandler, usernameInputResetHandler] =
         useInput(validateUsername);
     const usernameInputField = (
@@ -17,8 +18,8 @@ function AddMemberForm() {
     );
 
     return (
-        <div className='flex items-center min-h-[100vh] justify-center text-2xl bg-black bg-opacity-10'>
-            <div className='max-w-3xl w-full rounded-xl sm:p-12 p-6 grid gap-16 bg-white relative'>
+        <div onClick={props.onClose} className='flex items-center min-h-[100vh] w-full justify-center text-2x  absolute z-50 bg-black bg-opacity-30'>
+            <div className='max-w-3xl w-full rounded-xl sm:p-12 p-6 grid gap-16 bg-white relative' onClick={(event) => event.stopPropagation()}>
                 <form action='' className='grid gap-12 w-full'>
                     {usernameInputField}
                     <button className='rounded-lg bg-blue-700 py-3 text-blue-50 shadow-sm text-2xl disabled:bg-gray-500'>Add</button>
