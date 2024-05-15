7ff2346d25dcf449aa9b9b600007e9e9f7d241f9
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-15 15:33:26 +0300

    feat(group): add Add-member form to membersList
    - uses react-dom portal
    - not connected to backend yet

--- src/components/groupDashboard/members/membersList.tsx
+++ src/components/groupDashboard/members/membersList.tsx
@@ -1,4 +1,7 @@
+import { createPortal } from 'react-dom';
 import MembersListItem, { memberData } from './membersListItem';
+import AddMemberForm from './addMemberForm';
+import { startTransition, useState } from 'react';
 
 function putAdminFirst(members: memberData[]): void {
     const n = members.length;
@@ -13,10 +16,25 @@ function putAdminFirst(members: memberData[]): void {
     }
 }
 
-const addMemberButton = <button className='bg-blue-700 p-1 text-blue-50 font-medium w-full rounded-xl text-lg'>Add Member</button>;
 function MembersList(props: { members: memberData[] }) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     props.members.sort((a, b) => (a.username < b.username ? -1 : 1));
     putAdminFirst(props.members);
+
+    function toggleAddMemberForm() {
+        startTransition(() =>
+            setIsModalOpen((prev) => {
+                return !prev;
+            }),
+        );
+    }
+
+    const addMemberButton = (
+        <button className='bg-blue-700 p-1 text-blue-50 font-medium w-full rounded-xl text-lg' onClick={toggleAddMemberForm}>
+            Add Member
+        </button>
+    );
 
     return (
         <div className='shadow-lg rounded-xl px-12 py-6 mt-6 max-h-[100vh] overflow-y-scroll'>
@@ -26,6 +44,8 @@ function MembersList(props: { members: memberData[] }) {
                 ))}
             </ul>
             {addMemberButton}
+            {isModalOpen &&
+                createPortal(<AddMemberForm onClose={toggleAddMemberForm}></AddMemberForm>, document.getElementById('modal-root') as HTMLElement)}
         </div>
     );
 }
