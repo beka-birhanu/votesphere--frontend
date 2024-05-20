499fb8349ba28c762d4c0806b2db752a22f158b3
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-20 10:36:49 +0300

    feat(poll): add loading indicator in addPollForm
    - the save button change to saving
      and spining icon apears

--- src/components/groupDashboard/poll/addPollForm.tsx
+++ src/components/groupDashboard/poll/addPollForm.tsx
@@ -3,7 +3,35 @@ import AddPollInput from './addPollInput';
 
 const MAX_INPUT_SIZE = 6;
 
+function submit(question: string, options: string[], setIsLoading: CallableFunction) {
+    setIsLoading(true);
+    fetch('http://localhost:9000/polls', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            Authorization:
+                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJla2FfYWRtaW5AZ21haWwuY29tIiwidXNlcm5hbWUiOiJiZWthX2FkbWluIiwiaWF0IjoxNzE2MTQ2MTEzLCJleHAiOjE3MTYxNDk3MTN9.5iUTLqeYbHpqNVCKZk5fgnuZHCrBt6fEmiGm8oUHqvg',
+        },
+        body: JSON.stringify({ adminUsername: 'beka_admin', groupID: '830af378-238c-40da-9d4a-8793772f512e', poll: { question, options } }),
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log('Poll created successfully:', data);
+        })
+        .catch((error) => {
+            console.error('Error creating poll:', error);
+        });
+
+    setIsLoading(false);
+}
+
 function AddPollForm(props: { onClose: MouseEventHandler<HTMLElement> }) {
+    const [isLoading, setIsLoading] = useState(false);
     const [question, setQuestion] = useState('');
     const [option1, setOption1] = useState('');
     const [option2, setOption2] = useState('');
@@ -36,10 +64,39 @@ function AddPollForm(props: { onClose: MouseEventHandler<HTMLElement> }) {
         });
     }
 
+    function handleSubmit(event: React.FormEvent<HTMLElement>) {
+        event.preventDefault();
+        if (!canSubmit) {
+            return null;
+        }
+
+        let optionInputsData: string[] = [];
+        optionInputsData.push(option1);
+        optionInputsData.push(option2);
+
+        if (option3 !== '') {
+            optionInputsData.push(option3);
+        }
+        if (option4 !== '') {
+            optionInputsData.push(option4);
+        }
+        if (option5 !== '') {
+            optionInputsData.push(option5);
+        }
+        if (option6 !== '') {
+            optionInputsData.push(option6);
+        }
+
+        submit(question, optionInputsData, setIsLoading);
+    }
+
     return (
-        <div onClick={props.onClose} className='flex items-center min-h-[100vh] w-full justify-center text-2x  absolute z-50 bg-black bg-opacity-30'>
+        <div
+            onClick={props.onClose}
+            className={`flex items-center min-h-[100vh] w-full justify-center text-2x  absolute z-50 bg-black bg-opacity-30`}
+        >
             <div className='max-w-3xl w-full rounded-xl sm:p-12 p-6 grid gap-16 bg-white relative' onClick={(event) => event.stopPropagation()}>
-                <form action='' className='grid gap-12 w-full'>
+                <form action='' className='grid gap-12 w-full' onSubmit={handleSubmit}>
                     {questionInputField}
                     <div className='grid gap-10 w-full'>
                         {visibleOptionFields >= 1 && option1InputFiled}
@@ -49,8 +106,41 @@ function AddPollForm(props: { onClose: MouseEventHandler<HTMLElement> }) {
                         {visibleOptionFields >= 5 && option5InputFiled}
                         {visibleOptionFields >= 6 && option6InputFiled}
                     </div>
-                    <button className='rounded-lg bg-blue-700 py-3 text-blue-50 shadow-sm text-2xl disabled:bg-gray-500 mt-32' disabled={!canSubmit}>
-                        Save
+                    <button
+                        className='rounded-lg bg-blue-700 py-3 text-blue-50 shadow-sm text-2xl disabled:bg-gray-500 mt-32 flex justify-center gap-2 items-center'
+                        disabled={!canSubmit}
+                    >
+                        {isLoading && (
+                            <svg
+                                fill='#ffffff'
+                                version='1.1'
+                                id='Capa_1'
+                                xmlns='http://www.w3.org/2000/svg'
+                                width='16px'
+                                height='16px'
+                                viewBox='0 0 26.349 26.35'
+                                stroke='#ffffff'
+                                className='animate-spin'
+                            >
+                                <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+                                <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
+                                <g id='SVGRepo_iconCarrier'>
+                                    {' '}
+                                    <g>
+                                        {' '}
+                                        <g>
+                                            {' '}
+                                            <circle cx='13.792' cy='3.082' r='3.082'></circle> <circle cx='13.792' cy='24.501' r='1.849'></circle>{' '}
+                                            <circle cx='6.219' cy='6.218' r='2.774'></circle> <circle cx='21.365' cy='21.363' r='1.541'></circle>{' '}
+                                            <circle cx='3.082' cy='13.792' r='2.465'></circle> <circle cx='24.501' cy='13.791' r='1.232'></circle>{' '}
+                                            <path d='M4.694,19.84c-0.843,0.843-0.843,2.207,0,3.05c0.842,0.843,2.208,0.843,3.05,0c0.843-0.843,0.843-2.207,0-3.05 C6.902,18.996,5.537,18.988,4.694,19.84z'></path>{' '}
+                                            <circle cx='21.364' cy='6.218' r='0.924'></circle>{' '}
+                                        </g>{' '}
+                                    </g>{' '}
+                                </g>
+                            </svg>
+                        )}
+                        {isLoading ? 'Saving' : 'Save'}
                     </button>
                 </form>
 
