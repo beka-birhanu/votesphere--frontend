d64a3cce6b10bbb47d384305940dac933427b342
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-20 16:18:51 +0300

    refactor(group): make loadingSVG its own component

--- src/components/groupDashboard/members/addMemberForm.tsx
+++ src/components/groupDashboard/members/addMemberForm.tsx
@@ -2,6 +2,7 @@ import { MouseEventHandler, useState } from 'react';
 import useInput from '../../../hooks/use-input';
 import Input from '../../auth/authInput';
 import { validateUsername } from '../../auth/validators';
+import LoadingSVG from '../loadingSVG';
 
 function submit(username: string, setIsLoading: CallableFunction, setSubmitError: CallableFunction) {
     setIsLoading(true);
@@ -67,36 +68,7 @@ function AddMemberForm(props: { onClose: MouseEventHandler<HTMLElement> }) {
                 <form action='' className='grid gap-12 w-full' onSubmit={handleSubmit}>
                     {usernameInputField}
                     <button className='rounded-lg bg-blue-700 py-3 text-blue-50 shadow-sm text-2xl disabled:bg-gray-500 flex justify-center gap-2 items-center'>
-                        {isLoading && (
-                            <svg
-                                fill='#ffffff'
-                                version='1.1'
-                                id='Capa_1'
-                                xmlns='http://www.w3.org/2000/svg'
-                                width='16px'
-                                height='16px'
-                                viewBox='0 0 26.349 26.35'
-                                stroke='#ffffff'
-                                className='animate-spin'
-                            >
-                                <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
-                                <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
-                                <g id='SVGRepo_iconCarrier'>
-                                    {' '}
-                                    <g>
-                                        {' '}
-                                        <g>
-                                            {' '}
-                                            <circle cx='13.792' cy='3.082' r='3.082'></circle> <circle cx='13.792' cy='24.501' r='1.849'></circle>{' '}
-                                            <circle cx='6.219' cy='6.218' r='2.774'></circle> <circle cx='21.365' cy='21.363' r='1.541'></circle>{' '}
-                                            <circle cx='3.082' cy='13.792' r='2.465'></circle> <circle cx='24.501' cy='13.791' r='1.232'></circle>{' '}
-                                            <path d='M4.694,19.84c-0.843,0.843-0.843,2.207,0,3.05c0.842,0.843,2.208,0.843,3.05,0c0.843-0.843,0.843-2.207,0-3.05 C6.902,18.996,5.537,18.988,4.694,19.84z'></path>{' '}
-                                            <circle cx='21.364' cy='6.218' r='0.924'></circle>{' '}
-                                        </g>{' '}
-                                    </g>{' '}
-                                </g>
-                            </svg>
-                        )}
+                        {isLoading && <LoadingSVG></LoadingSVG>}
                         {isLoading ? 'Adding' : 'Add'}
                     </button>
                 </form>
