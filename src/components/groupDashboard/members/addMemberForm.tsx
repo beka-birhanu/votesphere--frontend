a94f79fec60683f06134e57dab6cd6a9ba34bc2c
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-20 21:25:16 +0300

    feat(group): update submit function in addMemberForm
    - close the modal if response is success
    - change implemntation to try catch
      instade of nested then and catch

--- src/components/groupDashboard/members/addMemberForm.tsx
+++ src/components/groupDashboard/members/addMemberForm.tsx
@@ -1,43 +1,41 @@
-import { MouseEventHandler, useState } from 'react';
+import { useState } from 'react';
 import useInput from '../../../hooks/use-input';
 import Input from '../../auth/authInput';
 import { validateUsername } from '../../auth/validators';
 import LoadingSVG from '../loadingSVG';
 
-function submit(username: string, setIsLoading: CallableFunction, setSubmitError: CallableFunction) {
+async function submit(username: string, setIsLoading: CallableFunction, setSubmitError: CallableFunction, closeModal: CallableFunction) {
     setIsLoading(true);
-
-    fetch('http://localhost:9000/groups/830af378-238c-40da-9d4a-8793772f512e/members', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            Authorization:
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJla2FfYWRtaW5AZ21haWwuY29tIiwidXNlcm5hbWUiOiJiZWthX2FkbWluIiwiaWF0IjoxNzE2MTkyODc2LCJleHAiOjE3MTYxOTY0NzZ9.poz5WxasEQO_WqguaLODxa35-XsiPYamWPKxb-q0IIw',
-        },
-        body: JSON.stringify({ username }),
-    })
-        .then((response) => {
-            if (!response.ok) {
-                if (response.status === 409) {
-                    setSubmitError('User belongs to other group');
-                } else if (response.status == 404) {
-                    setSubmitError('Invalid Username');
-                }
-                throw new Error(response.statusText);
-            }
-            return response.json();
-        })
-        .then((response) => {
-            console.log(response);
-        })
-        .catch((error) => {
-            console.log(error);
+    try {
+        const response = await fetch('http://localhost:9000/groups/830af378-238c-40da-9d4a-8793772f512e/members', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                Authorization:
+                    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJla2FfYWRtaW5AZ21haWwuY29tIiwidXNlcm5hbWUiOiJiZWthX2FkbWluIiwiaWF0IjoxNzE2MTkyODc2LCJleHAiOjE3MTYxOTY0NzZ9.poz5WxasEQO_WqguaLODxa35-XsiPYamWPKxb-q0IIw',
+            },
+            body: JSON.stringify({ username }),
         });
 
-    setIsLoading(false);
+        if (!response.ok) {
+            if (response.status === 404) {
+                setSubmitError('Invalid username');
+            } else if (response.status === 409) {
+                setSubmitError('User already belongs to another group');
+            } else {
+                throw new Error(response.statusText);
+            }
+        } else {
+            closeModal();
+        }
+    } catch (error) {
+        console.log(error);
+    } finally {
+        setIsLoading(false);
+    }
 }
 
-function AddMemberForm(props: { onClose: MouseEventHandler<HTMLElement> }) {
+function AddMemberForm(props: { onClose: CallableFunction }) {
     const [username, isUsernameInputTouched, usernameInputError, setUsername, usernameInputBlurHandler, usernameInputResetHandler] =
         useInput(validateUsername);
     const [isLoading, setIsLoading] = useState(false);
@@ -53,17 +51,22 @@ function AddMemberForm(props: { onClose: MouseEventHandler<HTMLElement> }) {
             isTouched={isUsernameInputTouched}
         ></Input>
     );
+
+    function handleClose(event: React.MouseEvent<HTMLElement>) {
+        props.onClose();
+    }
+
     function handleSubmit(event: React.FormEvent<HTMLElement>) {
         event.preventDefault();
         if (usernameInputError || isLoading) {
             return null;
         }
 
-        submit(username, setIsLoading, setSubmitError);
+        submit(username, setIsLoading, setSubmitError, props.onClose);
     }
 
     return (
-        <div onClick={props.onClose} className='flex items-center min-h-[100vh] w-full justify-center text-2x  absolute z-50 bg-black bg-opacity-30'>
+        <div onClick={handleClose} className='flex items-center min-h-[100vh] w-full justify-center text-2x  absolute z-50 bg-black bg-opacity-30'>
             <div className='max-w-3xl w-full rounded-xl sm:p-12 p-6 grid gap-16 bg-white relative' onClick={(event) => event.stopPropagation()}>
                 <form action='' className='grid gap-12 w-full' onSubmit={handleSubmit}>
                     {usernameInputField}
