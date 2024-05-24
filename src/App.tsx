dbafd06324c431b08fd2086dc02efc92a4cf3a7f
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-24 22:22:02 +0300

    feat(app): add initial routes in App.tsx for testing
    - Added basic route structure in App.tsx.
    - This is an initial setup for testing purposes and not the final implementation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './components/auth/signIn';
+import SignUp from './components/auth/signUp';
 import DashBoard from './components/groupDashboard/dashboard';
-import AddMemberForm from './components/groupDashboard/members/addMemberForm';
-import AddPollForm from './components/groupDashboard/poll/addPollForm';
+import PrivateRoute from './components/route/privateRoute';
 
 function App() {
-    return <DashBoard></DashBoard>;
+    return (
+        <Routes>
+            <Route path='/' element={<SignUp />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/login' element={<SignIn />} />
+            <Route
+                path='/dashboard'
+                element={
+                    <PrivateRoute>
+                        <DashBoard />
+                    </PrivateRoute>
+                }
+            />
+        </Routes>
+    );
 }
 
 export default App;
