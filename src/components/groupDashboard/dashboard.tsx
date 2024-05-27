3544cf4b05db9ab55e57f3b883ad48124bff3d90
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-27 11:02:00 +0300

    feat(group): display noGroup content if user has no group

--- src/components/groupDashboard/dashboard.tsx
+++ src/components/groupDashboard/dashboard.tsx
@@ -8,6 +8,7 @@ import DoubleArrowIcon from './icons/doubleArrowSVG';
 import { fetchPolls } from '../../API/poll';
 import { fetchMembers } from '../../API/members';
 import { useNavigate } from 'react-router-dom';
+import NoGroup from './noGroup';
 
 const UserDataContext = createContext({ groupID: '', username: '' });
 
@@ -51,30 +52,33 @@ function DashBoard() {
         <Fragment>
             <Header isAuthorized={true} isLoading={false}></Header>
             <main className='mt-32'>
-                <hgroup className='flex flex-col gap-6 items-center md:mb-16 mb-10'>
-                    <h1 className='text-blue-700 font-bold text-3xl uppercase'>{groupName}</h1>
-                    <p className='uppercase tracking-widest text-sm'>{motto}</p>
-                </hgroup>
+                {username && groupID && (
+                    <Fragment>
+                        <hgroup className='flex flex-col gap-6 items-center md:mb-16 mb-10'>
+                            <h1 className='text-blue-700 font-bold text-3xl uppercase'>{groupName}</h1>
+                            <p className='uppercase tracking-widest text-sm'>{motto}</p>
+                        </hgroup>
 
-                <div className='flex items-start min-w-96 w-full relative text-2xl'>
-                    {username && groupID && (
-                        <UserDataContext.Provider value={{ groupID, username }}>
-                            <div className='flex items-start min-w-96 w-full relative'>
-                                <PollList pollsData={pollsData} />
-                            </div>
-
-                            <aside className='min-w-72 absolute z-10 bg-white right-0 lg:mr-16'>
-                                <div className='flex items-center gap-2 ml-3'>
-                                    <button className='flex gap-0'>
-                                        <DoubleArrowIcon />
-                                    </button>
-                                    <h1>Members</h1>
+                        <div className='flex items-start min-w-96 w-full relative text-2xl'>
+                            <UserDataContext.Provider value={{ groupID, username }}>
+                                <div className='flex items-start min-w-96 w-full relative'>
+                                    <PollList pollsData={pollsData} />
                                 </div>
-                                <MembersList members={members} />
-                            </aside>
-                        </UserDataContext.Provider>
-                    )}
-                </div>
+
+                                <aside className='min-w-72 absolute z-10 bg-white right-0 lg:mr-16'>
+                                    <div className='flex items-center gap-2 ml-3'>
+                                        <button className='flex gap-0'>
+                                            <DoubleArrowIcon />
+                                        </button>
+                                        <h1>Members</h1>
+                                    </div>
+                                    <MembersList members={members} />
+                                </aside>
+                            </UserDataContext.Provider>
+                        </div>
+                    </Fragment>
+                )}
+                <NoGroup isAdmin={true}></NoGroup>
             </main>
         </Fragment>
     );
---
+++ src/components/groupDashboard/noGroup.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+function NoGroupForNonAdmin() {
+    return (
+        <div
+            className='flex w-full m-8
+        '
+        >
+            <div className='flex flex-col items-center gap-16 w-full'>
+                <h1 className='text-center text-4xl text-gray-300 font-bold'> Group Membership Required!</h1>
+                <p className='text-left text-2xl max-w-4xl'>
+                    You are currently not a member of any group. To join a group, please contact the administrator of the group you wish to join and
+                    request that they add you to their group. <br /> <br />
+                    <br />
+                    <strong className='font-normal'>
+                        <span className='font-semibold'>Please note</span>: You can only be a member of one group at a time.
+                    </strong>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+function NoGroupFroAdmin() {
+    const [groupName, setGroupName] = useState('');
+    const [groupMotto, setGroupMotto] = useState('');
+
+    return (
+        <div
+            className='flex w-full m-8
+        '
+        >
+            <div className='flex flex-col items-center gap-16 w-full'>
+                <h1 className='text-center text-4xl text-gray-300 font-bold'> Create Group!</h1>
+                <p className='text-left text-2xl max-w-4xl'>
+                    <strong className='font-normal'>
+                        <span className='font-semibold'>Please note</span>: You can only create one group.
+                    </strong>
+                </p>
+            </div>
+        </div>
+    );
+}
+function NoGroup(props: { isAdmin: boolean }) {
+    return props.isAdmin ? <NoGroupFroAdmin></NoGroupFroAdmin> : <NoGroupForNonAdmin></NoGroupForNonAdmin>;
+}
+
+export default NoGroup;
