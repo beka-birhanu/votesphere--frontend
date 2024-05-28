288edcac65f3ff9880e6a4457a3d0a23f0eb415e
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-28 14:53:08 +0300

    refactor(group): remove group motto input
    - It is not supported by the backend

--- src/components/groupDashboard/createGroupForm.tsx
+++ src/components/groupDashboard/createGroupForm.tsx
@@ -1,8 +1,27 @@
 import { MouseEventHandler, useContext, useEffect, useState } from 'react';
-import CreateGroupInput from './createGroupInput';
 import LoadingSVG from './icons/loadingSVG';
 
-async function submit(groupName: string, groupMotto: string, setIsLoading: CallableFunction) {
+function CreateGroupInput(props: { label: string; onChange: CallableFunction; value: string }) {
+    return (
+        <div className=' flex flex-col sm:flex-row justify-items-stretch text-xl '>
+            <label htmlFor={props.label} className='w-52 uppercase font-medium tracking-wide'>
+                {' '}
+                {props.label}:
+            </label>
+            <input
+                id={props.label}
+                type='text'
+                className='border-b-2 border-gray-400 focus:outline-none flex-grow w-full'
+                multiple={true}
+                value={props.value}
+                onChange={(event) => props.onChange(event.target.value)}
+                required={true}
+            />
+        </div>
+    );
+}
+
+async function submit(groupName: string, setIsLoading: CallableFunction) {
     setIsLoading(true);
 
     try {
@@ -20,16 +39,14 @@ function CreateGroupForm() {
     const [isLoading, setIsLoading] = useState(false);
 
     const [groupName, setGroupName] = useState('');
-    const [groupMotto, setGroupMotto] = useState('');
     const [canSubmit, setCanSubmit] = useState(false);
 
     const groupNameInputField = <CreateGroupInput value={groupName} onChange={setGroupName} label='group name'></CreateGroupInput>;
-    const groupMottoInputField = <CreateGroupInput value={groupMotto} onChange={setGroupMotto} label='group motto'></CreateGroupInput>;
 
     useEffect(() => {
-        const isRequirementFulfilled = groupName !== '' && groupMotto !== '';
+        const isRequirementFulfilled = groupName !== '';
         setCanSubmit(isRequirementFulfilled);
-    }, [groupName, groupMotto]);
+    }, [groupName]);
 
     function handleSubmit(event: React.FormEvent<HTMLElement>) {
         event.preventDefault();
@@ -37,14 +54,13 @@ function CreateGroupForm() {
             return null;
         }
 
-        submit(groupName, groupMotto, setIsLoading);
+        submit(groupName, setIsLoading);
     }
 
     return (
         <div className='max-w-3xl w-full rounded-xl sm:p-12 p-6 grid gap-16 bg-white relative' onClick={(event) => event.stopPropagation()}>
             <form action='' className='grid gap-12 w-full' onSubmit={handleSubmit}>
                 {groupNameInputField}
-                {groupMottoInputField}
                 <button
                     className='rounded-lg bg-blue-700 py-3 text-blue-50 shadow-sm text-2xl disabled:bg-gray-500 mt-32 flex justify-center gap-2 items-center'
                     disabled={!canSubmit}
